3289591bcdde72bc66aeb4653565e52d2ad36834
Your Name <you@example.com>
2024-07-13 21:13:20 +0530

    auth

--- src/util/ProtectedRouteUser.jsx
+++ src/util/ProtectedRouteUser.jsx
@@ -1,11 +1,32 @@
+// import { useNavigate } from "react-router-dom";
+
+// const isAuthenticated = () => {
+//     console.log(localStorage.getItem('access_token'));
+//     return localStorage.getItem('access_token') !== null
+// };
+
+// export const ProtectedRouteUser = ({ children }) => {
+//     const navigate = useNavigate();
+//     return isAuthenticated() ? children : navigate("/userlogin");
+// };
+
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const isAuthenticated = () => {
-    console.log(localStorage.getItem('access_token'));
-    return localStorage.getItem('access_token') !== null
+  console.log(localStorage.getItem('access_token'));
+  return localStorage.getItem('access_token') !== null;
 };
 
 export const ProtectedRouteUser = ({ children }) => {
-    const navigate = useNavigate();
-    return isAuthenticated() ? children : navigate("/userlogin");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log("userlogin");
+    if (!isAuthenticated()) {
+      navigate("/userlogin");
+    }
+  }, [navigate]);
+
+  return isAuthenticated() ? children : null;
 };
